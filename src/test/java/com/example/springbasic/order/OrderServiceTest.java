63a94091357fea14bb5319ddb419ecf1cc784cf3
dyddlfsla <7942dyddlfsl@naver.com>
2022-07-27 01:06:06 +0900

    refactor: AppConfig

--- src/test/java/com/example/springbasic/order/OrderServiceTest.java
+++ src/test/java/com/example/springbasic/order/OrderServiceTest.java
@@ -1,16 +1,24 @@
 package com.example.springbasic.order;
 
+import com.example.springbasic.AppConfig;
 import com.example.springbasic.member.Grade;
 import com.example.springbasic.member.Member;
 import com.example.springbasic.member.MemberService;
-import com.example.springbasic.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-  MemberService memberService = new MemberServiceImpl();
-  OrderService orderService = new OrderServiceImpl();
+  MemberService memberService;
+  OrderService orderService;
+
+  @BeforeEach
+  public void beforeEach() {
+    AppConfig appConfig = new AppConfig();
+    memberService = appConfig.memberService();
+    orderService = appConfig.orderService();
+  }
 
   @Test
   void createOrder() {
