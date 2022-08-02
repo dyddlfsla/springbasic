1c44792e38681cad9e06192d03bd5a924f32e451
dyddlfsla <7942dyddlfsl@naver.com>
2022-08-03 00:00:04 +0900

    add comment

--- src/main/java/com/example/springbasic/OrderApp.java
+++ src/main/java/com/example/springbasic/OrderApp.java
@@ -28,4 +28,11 @@ public class OrderApp {
     System.out.println("order.calculatePrice() = " + order.calculatePrice());
   }
 
+  /*
+  * 'ApplicationContext' 를 스프링 컨테이너라 한다.
+  * 기존에는 개발자가 'AppConfig' 를 사용해서 직접 객체를 생성하고 DI 를 했지만, 이제부터는 스프링 컨테이너를 통해서 사용한다.
+  * 스프링 컨테이너는 '@Configuration' 이 붙은 'AppConfig' 를 설정 정보로 사용한다.
+  * 여기서 @Bean 이라 적힌 메서드를 모두 호출한 뒤 반환된 객체를 스프링 컨테이너에 등록한다. 이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라 한다.
+  *
+  * */
 }
