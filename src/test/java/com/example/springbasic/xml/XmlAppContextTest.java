992d801af49eeb3442d7bcca2a74aabd1f3b9a4b
dyddlfsla <7942dyddlfsl@naver.com>
2022-08-09 00:23:25 +0900

    test: BeanDefinitionTest

--- src/test/java/com/example/springbasic/xml/XmlAppContextTest.java
+++ src/test/java/com/example/springbasic/xml/XmlAppContextTest.java
@@ -4,14 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.springbasic.member.MemberService;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class XmlAppContextTest {
 
   @Test
   void xmlAppContext() {
-    ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
     MemberService memberService = ac.getBean("memberService", MemberService.class);
     assertThat(memberService).isInstanceOf(MemberService.class);
   }
