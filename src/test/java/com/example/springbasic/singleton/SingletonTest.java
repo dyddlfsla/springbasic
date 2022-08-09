64484966b88a0270a3f703e19bda8d9c822c74ca
dyddlfsla <7942dyddlfsl@naver.com>
2022-08-09 23:42:24 +0900

    test: springContainer singleton

--- src/test/java/com/example/springbasic/singleton/SingletonTest.java
+++ src/test/java/com/example/springbasic/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.example.springbasic.AppConfig;
 import com.example.springbasic.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -29,6 +30,19 @@ public class SingletonTest {
     System.out.println("singletonService2 = " + singletonService2);
 
     assertThat(singletonService1).isSameAs(singletonService2);
+  }
+
+  @Test
+  void springContainer() {
+    AnnotationConfigApplicationContext appConfig =
+        new AnnotationConfigApplicationContext(AppConfig.class);
+    MemberService memberService1 = appConfig.getBean("memberService", MemberService.class);
+    MemberService memberService2 = appConfig.getBean("memberService", MemberService.class);
+
+    System.out.println("memberService1 = " + memberService1);
+    System.out.println("memberService2 = " + memberService2);
+
+    assertThat(memberService1).isSameAs(memberService2);
   }
 
 }
