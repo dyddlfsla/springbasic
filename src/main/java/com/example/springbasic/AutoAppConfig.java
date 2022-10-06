944a4ec6519fad0c7063491be370f175b3f1cd74
dyddlfsla <7942dyddlfsl@naver.com>
2022-10-06 23:51:10 +0900

    add comment

--- src/main/java/com/example/springbasic/AutoAppConfig.java
+++ src/main/java/com/example/springbasic/AutoAppConfig.java
@@ -15,4 +15,32 @@ public class AutoAppConfig {
 //    return new MemoryMemberRepository();
 //  }
 
+
+  /*
+  * ◆ 컴포넌트 스캔 - 컴포넌트 스캔과 의존관계 자동 주입
+  *
+  * 지금까지 스프링 빈을 등록할 때는 자바 코드의 @Bean 을 이용하거나 XML 의 <bean> 을 통해서 설정 정보에 직접 등록할 빈을 나열했다.
+  * 그러나 이렇게 등록해야할 스프링 빈이 점점 늘어나 수십, 수백개가 되면 일일이 등록하기도 귀찮고, 설정 정보도 커지고, 스프링 빈을 누락될수도 있는 문제가 발생한다.
+  * 그렇기 때문에 스프링 프레임워크에서는 설정 정보가 없어도 자동으로 스프링 빈을 등록할 수 있도록 컴포넌트 스캔이라는 기능을 제공한다.
+  * 또한 의존 관계를 표현할 수 있도록 @Autowired 라는 기능도 제공한다.
+  *
+  * 컴포넌트 스캔을 사용하려면 설정 정보 클래스에 @ComponentScan 이라는 애너테이션을 붙여주면 된다.
+  * 기존의 AppConfig 와는 다르게 @Bean 을 이용한 코드가 존재하지 않는다.
+  * ※ 원래대로라면 @Configuration 이 붙은 클래스도 스프링 빈으로 등록된다.
+  *    그러나 기존의 예제 코드를 최대한 남기기 위해 excludeFilter 를 사용하여 기존의 AppConfig 클래스와 그 내용이 스프링 빈으로 등록되지 않게 하였다.
+  *
+  * @ComponentScan 은 말 그대로 @Component 가 붙은 클래스들을 모두 찾아서 스프링 빈으로 등록한다.
+  *  스프링 빈으로 등록되어야 하는 클래스들(RateDiscountPolicy, MemberServiceImpl, MemoryMemberRepository, OrderServiceImpl)에게 @Component 를 붙여준다.
+  *
+  * 참고로, @Configuration 이 스캔의 대상이 되는 이유는 @Configuration 의 소스코드 안에 @Component 가 정의되어 있기 때문이다.
+  *
+  * 이전에 AppConfig 에서는 @Bean 애너테이션을 이용해 직접 설정 정보를 작성했고, 의존관계 역시 직접적으로 명시했다. 그러나 이제는 이러한 설정 정보 자체가 없기 때문에
+  * 의존관계 주입 역시 @Component 가 지정된 클래스 안에서 직접 해결해야 한다. 이를 대비해 @Autowired 를 사용할 수 있다.
+  *
+  *
+  *
+  *
+  *
+  *
+  * */
 }
--- src/test/java/com/example/springbasic/scan/AutoAppConfigTest.java
+++ src/test/java/com/example/springbasic/scan/AutoAppConfigTest.java
@@ -19,4 +19,21 @@ public class AutoAppConfigTest {
 
   }
 
+  /*
+  * new AnnotationConfigApplicationContext(AutoAppConfig.class);
+  * 설정 정보로 AppConfig.class 가 아닌 @ComponentScan 이 적용된 AutoAppConfig.class 를 넘겨준다.
+  * 스프링 빈 등록이 이전처럼 잘 되는 것을 확인할 수 있다.
+  *
+  * - @ComponentScan 과 의존관계 주입이 동작하는 과정
+  * 1. @ComponentScan 은 @Component 가 붙은 모든 클래스들을 찾아내어 스프링 빈으로 등록한다.
+  * 2. 이때 스프링 빈의 이름은 클래스명을 사용하되 맨처음 글자는 소문자로 변환하여 사용한다. 쉽게 말해, 클래스 이름에 캐멀 케이스를 적용한다.
+  *    또는 @Component("이름") 과 같은 형태로 이름을 직접 지정할 수도 있다.
+  * 3. 생성자에 @Autowired 를 지정하면, 스프링 컨테이너가 자동으로 해당 스프링 빈을 찾아서 의존관계를 주입한다. getBean(클래스타입) 과 동일하다고 할 수 있다.
+  *    생성자에 파라미터 수가 많더라도 다 찾아서 주입할 수 있다.
+  *
+  *
+  *
+  *
+  * */
+
 }
