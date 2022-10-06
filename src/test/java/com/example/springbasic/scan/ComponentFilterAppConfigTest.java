2c3c0c10c72c668430af178f50759731511d4964
dyddlfsla <7942dyddlfsl@naver.com>
2022-10-07 02:27:40 +0900

    add comment

--- src/test/java/com/example/springbasic/scan/ComponentFilterAppConfigTest.java
+++ src/test/java/com/example/springbasic/scan/ComponentFilterAppConfigTest.java
@@ -34,3 +34,13 @@ public class ComponentFilterAppConfigTest {
   )
   static class ComponentFilterAppConfig {}
 }
+
+/*
+* includeFilters 에 MyIncludeComponent 를 지정함으로써 @MyIncludeComponent 가 지정된 클래스를 컴포넌트 스캔 대상에 추가한다.
+* excludeFilters 에 MyExcludeComponent 를 지정함으로써 @MyExcludeComponent 가 지정된 클래스를 컴포넌트 스캔 대상에서 제외시켰다.
+*
+* ※ 일반적으로 @Component 애너테이션만으로도 충분하기 때문에 includeFilters 를 사용할 일은 거의 없다.
+*    excludeFilters 역시 간혹 사용하기는 하지만 그런 경우가 많지 않다.
+*
+*
+* */
