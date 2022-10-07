26975747db7f89ec4d73d85922b3bf1827afe0f9
dyddlfsla <7942dyddlfsl@naver.com>
2022-10-07 23:48:25 +0900

    feat: kind of injection

--- src/main/java/com/example/springbasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbasic/order/OrderServiceImpl.java
@@ -9,8 +9,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-  private final MemberRepository memberRepository;
-  private final DiscountPolicy discountPolicy;
+//  @Autowired private MemberRepository memberRepository;
+//  @Autowired private DiscountPolicy discountPolicy;
+
+  private MemberRepository memberRepository;
+  private DiscountPolicy discountPolicy;
+
+//  @Autowired
+//  public void setMemberRepository(MemberRepository memberRepository) {
+//    this.memberRepository = memberRepository;
+//  }
+//
+//  @Autowired
+//  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//    this.discountPolicy = discountPolicy;
+//  }
 
   @Autowired
   public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
