337c22fe498ffa0f83c09379ad0b6c219b0c24c6
dyddlfsla <7942dyddlfsl@naver.com>
2022-10-11 02:47:52 +0900

    feat: add comment

--- src/main/java/com/example/springbasic/discount/FixDiscountPolicy.java
+++ src/main/java/com/example/springbasic/discount/FixDiscountPolicy.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("fixDiscountPolicy")
+// ↑ @Qualifier 는 추가 구분자를 붙여주는 방식이다. 의존주입 시 추가적인 방법을 제공하는 것이지, 스프링 빈의 이름을 변경하는 것은 아니다.
+// 스프링 빈을 등록할 때 @Qualifier("이름") 을 붙여주고.
+// 의존 주입 받는 클래스로 가서 해당 매개변수 앞에도 같은 @Qualifier("이름") 을 붙여준다.
+// 이 애너테이션도 "이름" 에 해당하는 대상을 찾지 못하면 스프링 빈의 이름으로 다시 매칭하게 되는데, 이런 방식은 지양하는 것이 좋다.
+// @Qualifier 을 사용할 때에는 꼭 맞는 @Qualifier 를 짝지어서 사용하도록 하자.
+
+//@Primary
+// ↑ @Primary 는 우선 순위를 정하는 방법이다. @Autowired 시 여러 빈이 매칭되면 @Primary 가 붙은 스프링 빈이 우선권을 가지게 되는 것이다.
+
 public class FixDiscountPolicy implements DiscountPolicy {
 
   private int discountFixAmount = 1000;
@@ -18,3 +27,78 @@ public class FixDiscountPolicy implements DiscountPolicy {
     return 0;
   }
 }
+
+/*
+* ◆ 조회되는 Bean 이 2개인 경우 문제가 발생한다.
+*
+* 지금까지 사용했던 @Autowired 은 스프링 빈을 타입으로 조회한다.
+* 즉, getBean(DiscountPolicy.class) 처럼 동작한다는 것이다.
+* 앞서 배운 것처럼 스프링 빈을 타입(Type)으로 조회할 때 조회된 빈이 2개라면 문제가 발생한다.
+* DiscountPolicy 타입에 해당하는 빈은 fixDisCountPolicy 과 rateDiscountPolicy 이렇게 2개가 존재하므로 NoUniqueBeanDefinitionException 에러가 발생한다.
+* 이런 문제는 어떻게 해결해야 할까?
+* OrderServiceImpl 클래스의 자동 주입 코드를 다음과 같이 바꾸는 것은 어떨까?
+*
+*  private final MemberRepository memberRepository;
+*  private final RateDiscountPolicy discountPolicy;
+*
+*
+*  @Autowired
+*  public OrderServiceImpl(MemberRepository memberRepository, RateDiscountPolicy rateDiscountPolicy) {
+*    this.memberRepository = memberRepository;
+*    this.discountPolicy = discountPolicy;
+*  }
+*
+* 이렇게 바꾼다면 DiscountPolicy.class 타입으로 조회하는 것이 아니라 하위타입인 RateDiscountPolicy 타입으로 조회하게 되므로 문제는 해결될 것이다.
+* 그러나 이런 방식은 객체 지향 설계 원칙 중 DIP 를 위반하게 되는 것이고 유연성이 떨어진다. 또한 추후 이 하위타입의 클래스가 2개가 되면 같은 문제가 반복될 것이다.
+*
+* 올바른 해결 방식은 다음과 같다.
+*
+* 1. @Autowired 이 가진 필드명 매칭 기능 이용하기
+* 2. @Qualifier 사용하기
+* 3. @Primary 사용하기
+*
+* @Autowired 는 1차적으로 타입 매칭을 시도하고, 이때 빈이 2개 이상 존재하면 추가적으로 필드 이름과 파라미터 이름을 사용해 매칭을 시도한다.
+*
+* private final DiscountPolicy rateDiscountPolicy;
+*
+* 위와 같이 코드를 수정한다면, 필드명은 rateDiscountPolicy 가 되고 컨테이너는 등록된 스프링 빈 중 빈의 이름과 필드명이 일치하는 것을 찾아 매칭한다.
+* 즉 RateDiscountPolicy 의 구현체가 선택되어 주입될 것이다.
+* 이러한 필드명 매칭은 먼저 타입 매칭을 시도한 뒤 추가적으로 이루어지는 동작이다.
+*
+* Question. @Primary 와 @Qualifier 둘 중 어느 것을 사용해야 할까?
+* 애플리케이션 구조에서 자주 사용하는 메인 DB 에 접근하는 스프링 빈이 있고, 자주 사용되지는 않고 가끔 사용되는 서브 DB 에 접근하는 스프링 빈이 있다고 하자.
+* 이런 경우, 메인 DB 에 접근하는 빈에는 @Primary 를 사용하여 간편하게 사용하고, 서브 DB 에 접근하는 빈에는 @Qualifier 를 사용해 명시적으로 표현하면
+* 코드를 깔끔하게 유지할 수 있다.
+*
+* Question. @Primary 와 @Qualifier 이 동시에 설정되어 있다면 어느 것이 우선권을 가질까?
+* @Primary 는 기본값처럼 동작하는 것이고 @Qualifier 는 매우 상세적으로 동작한다. 스프링은 자동보다는 수동이, 넓은 범위의 선택보다는 좁은 범위의 선택이 우선권을 가지게 된다.
+* 따라서 여기서는 좁고 명시적인 의미를 가진 @Qualifier 가 우선권을 가지게 된다.
+*
+* ※ @Qualifier 을 내부적으로 포함하는 애너테이션 만들기.
+* @Qualifier 를 그냥 사용하는 경우, 오타가 발생해도 컴파일이 감지하지 못하는데, 이때 별도의 애너테이션을 만들어 해결할 수도 있다.
+*
+* @Qualifier("...")
+* ...
+* @interface MainDiscountPolicy {}
+*
+* 위의 코드와 같이 내부적으로 @Qualifier 를 가진 사용자 애너테이션을 만들어서 사용하면 오타가 발생했을 때 컴파일 에러가 발생한다.
+* 단, 애너테이션이 다른 애너테이션을 내부적으로 상속받는 것은, 자바의 순수 기능이 아니라 스프링 프레임워크가 지원하는 기능이다.
+*
+* Question. 수동 빈 등록 vs 자동 빈 등록 어떻게 구분해서 사용해야 할까?
+* 애플리케이션이 가지고 있는 로직은 크게 업무 로직과 기술 지원 로직으로 나눌 수 있다.
+* 업무 로직은 숫자도 매우 많고, 한번 개발해야 하면 컨트롤러, 서비스, 레퍼지토리 등 유사한 패턴이 있다. 이럴 때 자동 빈 등록 기능을 이용하는 것이 좋다.
+* 보통 문제가 발생해도 어떤 곳에서 발생하는지 명확하게 파악하기 쉽다.
+*
+* 기술 지원 로직은 업무 로직에 비교해서 그 수가 매우 적고, 보통 애플리케이션 전반에 걸쳐서 광범위하게 영향을 미친다. 또한 기술 지원 로직은
+* 문제가 발생해도 잘 드러나지 않고 파악하기도 어려운 경우가 많다. 그래서 이런 기술지원 로직들은 가급적이면 수동 빈 등록을 사용해서 명확하게 겉으로 드러내는 것이 좋다.
+*
+* => 자동 빈 등록을 기본으로 사용하되,
+*    애플리케이션에 전범위적으로 영향을 주는 기술지원 객체는 수동으로 등록하여 설정 정보에 명시적으로 드러나게 하는 것이 유지보수에 좋다.
+*
+*
+*
+*
+*
+*
+*
+* */
--- src/main/java/com/example/springbasic/order/OrderServiceImpl.java
+++ src/main/java/com/example/springbasic/order/OrderServiceImpl.java
@@ -8,20 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor ← 이 애너테이션은 클래스가 가진 필드 중 final 이 붙은 필드들을 모아서 생성자를 자동으로 만들어준다..!
+// ◆ lombok 과 최신 트렌드
+// 실무에서 개발을 진행하다 보면, 대부분의 의존관계가 다 불변이고 그에 따라 final 키워드를 사용하게 된다.
+// 그런데 매번 생성자로 만들어야 하고, 주입 받은 값을 대입하는 코드도 만들어야 하고..
+// 지루하고 반복적인 작업은 개발자를 힘들게 한다..
+// 그러나 이런 상황에서 반복적인 코드를 대신 만들어주는 라이브러리가 있다면 어떨까?
+// lombok 을 적용해보자. 코드가 정말 간결해진다.
+// lombok 은 자바의 애너테이션 프로세서라는 기능을 이용해서 컴파일 시점에 자바 코드를 대신 생성해준다.
+// 실제로 컴파일된 파일을 살펴보면 코드가 생성되어 있는 것을 확인할 수 있다.
+// 최근 애플리케이션 개발에서는 클래스에 생성자를 하나만 만들고 이에 따라 @Autowired 를 생략하는 방법을 주로 사용한다.
 public class OrderServiceImpl implements OrderService{
 
 //  @Autowired private MemberRepository memberRepository;
-//  @Autowired private DiscountPolicy discountPolicy;
+//  @Autowired private DiscountPolicy discountPolicy;ㅁ
+//  ↑ 필드 주입 방식
 
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
 
-  @Autowired
+
+  @Autowired // ← @Autowired 은 기본적으로 주입할 대상이 없으면 오류를 발생시킨다. 주입할 대상이 없어도 동작하게 하려면 @Autowired(required = false) 를 지정한다.
   public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
-  //  @Autowired
+  // ↑ 생성자 주입 방식. 말그대로 생성자를 통해 의존관계를 주입하고 있다.
+  // 생성자가 하나만 존재하는 상황이라면 @Autowired 애너테이션을 생략할 수도 있다.
+
+//  @Autowired
 //  public void setMemberRepository(MemberRepository memberRepository) {
 //    this.memberRepository = memberRepository;
 //  }
@@ -30,6 +45,14 @@ public class OrderServiceImpl implements OrderService{
 //  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //    this.discountPolicy = discountPolicy;
 //  }
+//  ↑ 수정자(setter) 주입. setter 메서드라 불리는 메서드를 이용해 의존관계를 주입하는 방식이다.
+
+
+//  public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//    this.memberRepository = memberRepository;
+//    this.discountPolicy = discountPolicy;
+//  }
+//  ↑ 일반 메서드를 이용해 주입하는 방식. 4가지 방식 중 가장 사용되지 않는다.
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -55,3 +78,75 @@ public class OrderServiceImpl implements OrderService{
  * OrderServiceImpl 에는 MemoryMemberRepository, FixDiscountPolicy 객체의 의존관계가 주입된다.
  *
  * */
+
+
+/*
+* ◆ 의존관계 자동 주입
+*
+* 다양한 의존관계 주입 방법
+* 의존관계 주입에는 크게 4가지 방법이 존재한다.
+*
+* 1. 생성자 주입
+* 생성자 주입은 말 그대로 생성자를 통해서 의존관계를 주입하는 방법이다.
+* 지금까지 진행해왔던 방식이 바로 이 방식인 것이다.
+* 생성자 주입은 다음과 같은 특징을 가지고 있다.
+* => 생성자 호출 시점에 딱 한번만 의존관계가 주입된다.
+* => 불변, 필수와 같은 의존관계에 사용한다
+*
+* 2. 수정자 주입
+* setter 메서드를 이용해 의존관계를 주입하는 방식이다.
+* => 선택, 변경 가능성이 있는 의존관계에서 사용한다.
+* => 자바빈 프로퍼티 규약의 수정자 메서드 방식을 이용하는 방법이다.
+*
+* 3. 필드 주입
+* 별도의 메서드를 사용하지 않고 필드 그 자체에 바로 주입하는 방식이다.
+* => 다른 주입에 비해 코드가 아주 간결하므로 좋은 방식처럼 보이지만, 실제로는 외부에서의 변경 가능성을 아예 차단하므로 단점이 발생한다.
+* => 의존 주입을 오로지 스프링 프레임워크에 맡기는 방식이므로 개발자가 주입 과정에 전혀 개입할 수 없다.
+* => 예를 들어, 테스트 환경에서 임시로 주입 객체를 바꾸고 싶어도 바꿀 수가 없다.
+* => 사용하지 않는 것이 권장된다.
+*
+* 4. 일반 메서드 주입
+* 클래스가 가진 일반 메서드를 이용해 의존 주입하는 방식이다.
+* => 한번에 여러 필드를 주입 받을 수 있다.
+* => 대체적으로 잘 사용되지 않는다.
+*
+*
+* ◆ 옵션 처리
+*
+* 프로그램을 개발하다 보면 스프링 빈이 주입되지 않아도 동작해야 하는 경우가 있다.
+* 그런데 @Autowired 을 이용해서 의존 주입을 하게되면 자동 주입 대상이 없을 때 오류가 발생하도록 되어 있다.
+* 이런 경우, 주입 되는 대상이 존재하는지에 따라 별도의 처리를 추가할 수 있다.
+*
+* 1) @Autowired(require = false): 자동 주입할 대상이 존재하지 않으면 수정자 메서드 자체가 호출되지 않고 에러가 발생하지 않는다.
+* 2) @Nullable: 주입 받는 참조변수 타입에 붙여주면 자동 주입할 대상이 없을 시 null 이 입력된다.
+* 3) Optional<T>: Optional 래퍼 클래스를 사용하면 자동 주입할 대상이 없을 시 Optional.empty() 가 호출된다.
+*
+*
+* ◆ 4가지의 의존관계 주입 방식 중 어떤 것을 사용해야 할까?
+*
+* => 항상 생성자 주입을 선택해라!
+* 과거에는 수정자 주입과 필드 주입을 많이 사용했지만 최근에는 스프링을 포함한 DI 프레임워크 대부분이 생성자 주입을 권장한다.
+* 1) 대부분의 의존관계는 한번 주입되고 나면 애플리케이션 종료시점까지 다시 변경될 일이 없다. 오히려 한번 설정된 의존관계는 변경되면 안된다.(불변해야 한다)
+* 2) 수정자 주입을 사용한다면, 불필요한 setter 메서드를 만들고 또 public 으로 열어두기까지 해야한다. setter 메세드를 만드는 건 항상 신중해야 하며 저런 방식은 옳지 못하다.
+* 3) 생성자 주입의 경우, 객체를 생성할 때 딱 한번 주입되고 그 이후 주입되지 않으므로, 불변하게 설계할 수 있다.
+* 4) 생성자 주입을 사용하면 부수적으로 final 키워드도 사용할 수 있게 되고, 추후 생성자에서 값이 설정되지 않는 오류를 컴파일 시점에서 미리 막을 수 있다. (에러 중 가장 좋은 에러는 컴파일 시점에서 발생하는 오류이다.)
+*    ※ 수정자 주입을 포함한 나머지 주입 방식들은 모두 생성자 호출 이후 별도로 호출되므로, 필드에 final 키워드를 사용할 수 없다. 오로지 생성자 주입만이 final 키워드를 사용할 수 있다.
+*
+* 생성자 주입 방식을 선택하는 이유는 여러 가지가 있지만, 생성자 주입 방식은 프레임워크에 의존하지 않고, 순수한 자바의 특징을 잘 살리는 방법이기도 하다.
+* 애플리케이션을 설계할 때에는, 기본적으로 생성자 주입 방식을 사용하고, 필수 값이 아닌 객체에 대해서는 수정자 주입 방식을 이용해서 선택적으로 주입하면 된다.
+*
+* 항상 생성자 주입을 선택해라! 그리고 가끔 선택적 처리가 필요하다면 그때 수정자 주입을 사용하라.
+* 필드 주입은 절대 사용하지 않는다!
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
